c448215364e14ac83c589f365a9cfcd32143a833
aixamercapidez <aixamercapidez@hotmail.com>
2023-10-08 21:40:54 -0300

    fin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const passport = require('passport')
+
 const productRouter = require('./products.router.js')
 const cartrouter = require('./carts.router.js')
 const messageRouter = require('./message.router.js')
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,21 +1,4 @@
-// const { Router } = require('express');
-// const uploader = require("../utils/multer");
-// const router = Router()
-// const{updateRol, document, getUsers, deleteUsers, deleteUser}=require("../controllers/users.controller")
 
-// router.get('/', getUsers)
-// router.get('/premium/:UID', updateRol)
-// router.post('/:UID/documents', uploader.fields([
-//     { name: "profile", maxCount: 1 },
-//     { name: "products", maxCount: 1 },
-//     { name: "identification", maxCount: 1 },
-//     { name: "comprobant", maxCount: 1 },
-//     { name: "accountStatus", maxCount: 1 },
-// ]),document)
-// router.delete('/deleteusers', deleteUsers)
-// router.get('/delete/:UID', deleteUser)
-
-// module.exports = router
 
 const UserController = require("../controllers/users.controller");
 const uploader = require("../utils/multer");
@@ -26,7 +9,7 @@ const user = new UserController()
 
 class UsersRouter extends RouterClass {
     init() {
-        this.get('/', ['ADMIN'], user.getUsers)//Funciona
+        this.get('/', ['ADMIN'], user.getUsers)
         this.get('/premium/:UID', ['USER', 'PREMIUM', 'ADMIN'], user.updateRol)
         this.post('/:UID/documents', ['USER', 'PREMIUM', 'ADMIN'], uploader.fields([
             { name: "profile", maxCount: 1 },
@@ -35,9 +18,9 @@ class UsersRouter extends RouterClass {
             { name: "comprobant", maxCount: 1 },
             { name: "accountStatus", maxCount: 1 },
         ]), user.document)
-        this.delete('/deleteusers', ['ADMIN'], user.deleteUsers)//funciona
+        this.delete('/deleteusers', ['ADMIN'], user.deleteUsers)
 
-        this.get('/delete/:UID', ['ADMIN'], user.deleteUser)//Funciona
+        this.get('/delete/:UID', ['ADMIN'], user.deleteUser)
 
 
 
